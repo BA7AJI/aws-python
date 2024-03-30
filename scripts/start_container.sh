d2e41c3ceb3218de70ca2d32e436d8e22dd99ca5
BA7AJI <121872586+BA7AJI@users.noreply.github.com>
2024-03-30 12:19:42 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker docker pull ba7aji/simple-python-app
+docker pull ba7aji/simple-python-app
 
 # Run the Docker image as a container
 docker run -d -p 5000:5000 ba7aji/simple-python-app
